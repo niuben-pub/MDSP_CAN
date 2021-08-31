b78712b3fcf398fb2d987e83c8238e507cfa8661
niuben <784313111@qq.com>
2021-08-31 16:47:48 +0800

    修改为双层Tab

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -27,7 +27,7 @@ public:
         QSize s = QProxyStyle::sizeFromContents(type, option, size, widget);
         if (type == QStyle::CT_TabBarTab) {
             s.transpose();
-            s.rwidth() = 160; // 设置每个tabBar中item的大小
+            s.rwidth() = 140; // 设置每个tabBar中item的大小
             s.rheight() =50;
         }
         return s;
@@ -66,6 +66,54 @@ public:
     }
 };
 
+
+class SubCustomTabStyle : public QProxyStyle
+{
+public:
+    QSize sizeFromContents(ContentsType type, const QStyleOption *option,
+        const QSize &size, const QWidget *widget) const
+    {
+        QSize s = QProxyStyle::sizeFromContents(type, option, size, widget);
+        if (type == QStyle::CT_TabBarTab) {
+            s.transpose();
+            s.rwidth() = 120; // 设置每个tabBar中item的大小
+            s.rheight() =40;
+        }
+        return s;
+    }
+
+    void drawControl(ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
+    {
+        if (element == CE_TabBarTabLabel) {
+            if (const QStyleOptionTab *tab = qstyleoption_cast<const QStyleOptionTab *>(option)) {
+                QRect allRect = tab->rect;
+
+                if (tab->state & QStyle::State_Selected) {
+                    painter->save();
+                    painter->setPen(0xF8CBAD);
+                    painter->setBrush(QBrush(0xF8CBAD));
+                    painter->drawRect(allRect.adjusted(6, 6, -6, -6));
+                    painter->restore();
+                }
+                QTextOption option;
+                option.setAlignment(Qt::AlignCenter);
+                if (tab->state & QStyle::State_Selected) {
+                    painter->setPen(0xf8fcff);
+                }
+                else {
+                    painter->setPen(0x5d5d5d);
+                }
+
+                painter->drawText(allRect, tab->text, option);
+                return;
+            }
+        }
+
+        if (element == CE_TabBarTab) {
+            QProxyStyle::drawControl(element, option, painter, widget);
+        }
+    }
+};
 
 void MainWindow::sleep(unsigned int msec)
 {
@@ -115,6 +163,10 @@ void MainWindow::initUi()
     this->setWindowTitle("MDSP-CAN Tool V1.0");
     ui->tabWidget->setCurrentIndex(0);//设置选中第一个标签
     ui->tabWidget->tabBar()->setStyle(new CustomTabStyle);
+    ui->tabWidget_2->tabBar()->setStyle(new SubCustomTabStyle);
+    ui->tabWidget_3->tabBar()->setStyle(new SubCustomTabStyle);
+    ui->tabWidget_4->tabBar()->setStyle(new SubCustomTabStyle);
+    ui->tabWidget_6->tabBar()->setStyle(new SubCustomTabStyle);
     ui->lcdNumberCurrent->setDigitCount(4);
     ui->lcdNumberVoltage12->setDigitCount(4);
     ui->lcdNumberVoltage5->setDigitCount(4);
